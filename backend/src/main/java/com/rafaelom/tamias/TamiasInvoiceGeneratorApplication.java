3b63de925bdcd00112d72562cc6f19bbe4ecddfc
Rafael Ordoñez <rafaelordonezmolina@gmail.com>
2023-07-08 22:37:21 +0200

    First commit

---
+++ backend/src/main/java/com/rafaelom/tamias/TamiasInvoiceGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelom.tamias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TamiasInvoiceGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TamiasInvoiceGeneratorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/rafaelom/tamias/TamiasInvoiceGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelom.tamias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TamiasInvoiceGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
